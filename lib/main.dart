f9f934d1c5856707a64a8eaec3ba13ed03f2b4d9
Bruno Jungton <bruno_jungton@hotmail.com>
2023-12-05 21:57:49 -0300

    Future Builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:sky_engine/_http/http.dart' as http;
-import 'package:async/async.dart';
-import 'dart:convert';
+//import 'package:sky_engine/_http/http.dart' as http;
+//import 'dart:async';
+//import 'dart:convert';
 
-const request = "https://api.hgbrasil.com/finance/quotations?key=233e6230";
+//const request = "https://api.hgbrasil.com/finance/quotations?key=233e6230";
 
 void main() async {
-  // http.HttpResponse response = await http.get(request);
-  //json.decode(response.body);
-  runApp(MaterialApp(home: Container()));
+  runApp(const MaterialApp(home: Home()));
 }
 
-class MyWidget extends StatefulWidget {
-  const MyWidget({super.key});
+/*Future<Map> getData() async {
+  http.HttpResponse response = await http.get(request);
+  return json.decode(response.body);
+}
+*/
+class Home extends StatefulWidget {
+  const Home({super.key});
 
   @override
-  State<MyWidget> createState() => _MyWidgetState();
+  State<Home> createState() => _HomeState();
 }
 
-class _MyWidgetState extends State<MyWidget> {
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text("\$ Conversor \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: FutureBuilder<Map>(
+          //    future: getData(),
+          builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            return const Center(
+                child: Text(
+              "Carregando Dados...",
+              style: TextStyle(color: Colors.amber, fontSize: 25.0),
+              textAlign: TextAlign.center,
+            ));
+          default:
+            if (snapshot.hasError) {
+              return const Center(
+                  child: Text(
+                "Erro ao Carregar Dados",
+                style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                textAlign: TextAlign.center,
+              ));
+            } else {
+              return Container(
+                color: Colors.green,
+              );
+            }
+        }
+      }),
+    );
   }
 }
